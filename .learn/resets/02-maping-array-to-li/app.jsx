1ce8ce1a1d666726d76ddcaba11eb1361aa845bb
FraanMoore <fraanmoore@gmail.com>
2023-01-26 23:52:04 +0000

    ejercicios de REACT terminados

--- .learn/resets/02-maping-array-to-li/app.jsx
+++ .learn/resets/02-maping-array-to-li/app.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const animals = ["Horse", "Turtle","Elephant","Monkey"];
+const animals = ["Horse", "Turtle", "Elephant", "Monkey"];
 
 /**
  * change the content inside the map function
  **/
 const animalsInHTML = animals.map((singleAnimal, i) => {
-	return <li key={i}>{singleAnimal}</li>;
+	return <li>hello</li>;
 });
 
 ReactDOM.render(<ul>{animalsInHTML}</ul>, document.querySelector("#myDiv"));
